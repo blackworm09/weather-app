c9cbd7fa3a95ae9e3f08478ee689d138dd6ed13e
Elena <sweatpants4president@gmail.com>
2021-07-09 19:32:46 +0200

    showed date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes <10) {
+    minutes = `0${minutes}`;
+
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +29,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windELement = document.querySelector("#wind");
+  let dateELement = document.querySelector("#date");
   temperatureElement.innerHTML = math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = math.round(response.data.wind.speed);
+  dateELement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "335600f837dbc92edc2599da33a2cd89";
