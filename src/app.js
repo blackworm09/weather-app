a5938dea29e964de6a71306cbc4afdc195830ffd
Elena <sweatpants4president@gmail.com>
2021-07-09 18:35:49 +0200

    implemented axios

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 function displayTemperature(response) {
-  
-  let temperatureElement=document.querySelector("#temperature");
-  temperatureElement.innerHTML=math.round(response.data.main.temp);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = math.round(response.data.main.temp);
+  cityElement.innerHTML=response.data.name;
 }
 
-let apiKey = "d3b0a871434ba42ee376c0c2172f7162";
+let apiKey = "29d488da5f7c3e2585b5dc3f2dd7f9f2";
 let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metrics`;
 
 axios.get(apiUrl).then(displayTemperature);
