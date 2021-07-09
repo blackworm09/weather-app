843153096ede8eb4396d5e4aaf39d3cd3a3082a6
Elena <sweatpants4president@gmail.com>
2021-07-09 19:12:32 +0200

    showed temperature

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windELement = document.querySelector("#wind");
   temperatureElement.innerHTML = math.round(response.data.main.temp);
-  cityElement.innerHTML=response.data.name;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = math.round(response.data.wind.speed);
 }
 
-let apiKey = "29d488da5f7c3e2585b5dc3f2dd7f9f2";
+let apiKey = "335600f837dbc92edc2599da33a2cd89";
 let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metrics`;
 
 axios.get(apiUrl).then(displayTemperature);
